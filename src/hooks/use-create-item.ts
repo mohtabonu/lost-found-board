33c8b13f24cdaf10174114e78f028e627151536c
бону <qudratovamohtabonu@gmail.com>
2025-08-10 13:59:44 +0500

    fix error

--- src/hooks/use-create-item.ts
+++ src/hooks/use-create-item.ts
@@ -1,17 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { http } from "@/services";
 import type { Item } from "@/types";
-import { useContext } from "react";
-import { AuthContext } from "@/context";
 
 export const useCreateItem = () => {
   const queryClient = useQueryClient();
-  const { user } = useContext(AuthContext);
 
   return useMutation({
     mutationFn: async (newItem: Omit<Item, "id" | "createdAt">) => {
-      if (!user?.id) throw new Error("User is not logged in");
-      const itemWithUser = { ...newItem, userId: user.id };
+      const itemWithUser = { ...newItem};
       const response = await http.post("/items", itemWithUser);
       return response.data as Item;
     },
